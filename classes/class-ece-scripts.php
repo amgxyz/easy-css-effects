cb51f3f10b43d490bb2160c5feda384f23bbce8f
Andy <andrewmgunn26@gmail.com>
2016-07-20 11:42:21 -0500

    lll

--- classes/class-ece-scripts.php
+++ classes/class-ece-scripts.php
@@ -16,7 +16,7 @@ class ECE_Script_Handler {
 
 		global $poop;
 
-		//add_action('wp_footer', array( $this,'ece_scripts' ),5 );
+		add_action('wp_footer', array( $this,'ece_scripts' ),5 );
 		//add_action('wp_footer', array( $this,'ece_styles' ),5 );
 
 		//var_dump($poop);
@@ -27,7 +27,16 @@ class ECE_Script_Handler {
 		        
 		        //jQuery(document).ready(function($) {
 		        jQuery(document).ready(function($){
-		        	
+		        	$('.flexslider').flexslider();/*{
+				        animation: "slide",
+				        animationLoop: false,
+				        itemWidth: 210,
+				        itemMargin: 5,
+				        pausePlay: true,
+				        start: function(slider){
+				          $('body').removeClass('loading');
+				        }
+			      	});*/
 
 				});
 
