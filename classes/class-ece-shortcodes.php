90fa1ddbfdcc523994357d7e9668d60916476e27
Andy <andrewmgunn26@gmail.com>
2016-07-22 15:00:57 -0500

    vvsdvsdvsd

--- classes/class-ece-shortcodes.php
+++ classes/class-ece-shortcodes.php
@@ -30,7 +30,9 @@ your theme's stylesheet content.
 	private $tag_arr = array();
 	private $param_arr = array(); 
 	private $shortcodes = array();
+	private $params = array();
 	private $elements = array();
+	private $obj_arr = array();
 	private $elem_arr = array('a','area','article','aside','body','blockquote','button','caption','code','div','dl','dt','embed','fieldset','figure','form','frame','framset','head','header','iframe','img','input','label','optgroup','option','p','pre','section','span','strong','sub','sup','table','tbody','td','th','tr','textare','ul','li');
 	private $class_arr = array(
 				'spin90-right', 'spin180-right', 'spin540-right' , 'spin900-right' , 'spin360-right' ,
@@ -40,7 +42,18 @@ your theme's stylesheet content.
 
 
 	public function __construct() {
+
 		add_shortcode( 'css_effect', array( $this, 'ece_params_shortcode' ) );
+
+		$this->ece_params_shortcode(null);
+		$objects = $this->ece_get_class_array();
+		var_dump($objects);
+		foreach($objects as $obj) {
+			add_shortcode( $obj, array( $this, 'ece_class_shortcode' ) );
+			array_push($this->effect_arr, $obj);
+		}
+
+		var_dump($this->effect_arr);
 	
 	}
 	public function ece_params_shortcode( $atts, $content = null ) {
@@ -66,30 +79,10 @@ your theme's stylesheet content.
 	        'height' => '150',
 	        'action' => 'POST',
 	        'width' => '150',
-	        'style' => 'margin:0 auto;',
-	        'attr' => ' title="'.$this->tag_id.$sp.
-	        				'id="'.$this->tag_id.'"'.$sp.
-	        				'name="'.$this->tag_id.'"'.$sp
+	        'style' => 'margin:0 auto;'
 	        				//'class="spin360"'.$sp;
 	    ), $atts );
 
-	    $tag = esc_attr( $params['element'] );
-	    $this->params['element'] = $tag;
-
-	    $class = esc_attr( $params['class'] );
-	    $this->params['class'] = $class;
-
-	    if ( $params['direction'] !== null && $params['direction'] !== '' ){ 
-	    	$dir = esc_attr( $params['direction'] );
-	    	$this->params['class'] = $class.$ds.$direction;
-	    } else {
-	    	$this->params['class'] = $class;
-	    }
-	    
-	    $attr_attr = esc_attr( $params['attr'] );
-	    $attr_html = esc_html( $attr_attr );
-	    $attr_url = esc_url( $attr_html );
-	    $this->params['attr'] = $attr_url;
 
 	    $style_attr = esc_attr( $params['style'] );
 	    $style_url = esc_url( $style_attr );
@@ -98,116 +91,117 @@ your theme's stylesheet content.
 
 
 
-	    if ( $tag !== null && $tag !== false && $tag !== false ) {
-	    	$str = $this->ece_parse_tag( $tag, $params );
+	    if ( $this->params['element'] !== null &&
+	    	 $this->params['element'] !== false && $this->params['element'] !== false ) {
+	    	$str = $this->ece_parse_params_shortcode( $this->params['element'], $this->params );
 		}
 
-		$shortcode = $this->str;
+		var_dump($this->params);
+		var_dump($str);
 
+		$shortcode = $this->str;
 		return $shortcode;
 	
 	}
 
-	public function ece_class_shortcodes() {
+	public function ece_element_builder() {
+		$sp = ' ';
+		$ds = '-';
+		$us = '_';
+		$elements = $this->elem_arr;
+		$classes = $this->class_arr;
+		$shortcode = "";
 
-	    /*$content = 'HTML elements you want effect applied to.';
-	    $this->tag = esc_attr( $params['element'] );
-	    $this->tag_id = esc_attr( 'ece_'.date("YmdHis") );
-	    $this->tag_class = esc_attr( $params['class'] );
+		foreach( $elements as $elem ) {
+			//$tag = $elem;
 
-    	$this->str = '<'. $tag . ' 
-    					id="'. $tag_id .'" 
-				    	class="' . $tag_class . '" >'.
-				    	$content
-				    	.'</'. $tag .'>';
+			foreach( $classes as $class ) {
+				$class_dir = explode( '-', $class );
 
-		if ( is_array($this->elem_arr) &&
-			 $this->elem_arr !== null && $this->elem_arr !== '') {
+				if ( $class_dir[0] !== null && $class_dir[0] !== '' ) {
+					$shortcode = $elem.$us.$class_dir[0];
 
-			foreach ( $elem_arr as $elem ) {
-				$tag = esc_attr( $params['element'] );
-			    $tag_id = esc_attr( 'ece_'.date("YmdHis") );
-			    $tag_class = esc_attr( $params['class'] );
+					if ( $class_dir[1] !== null && $class_dir[1] !== '' && $class_dir[1] !== false ) {
+						$shortcode = $elem.$us.$class_dir[0].$us.$class_dir[1];
+					}
+					$this->ece_parse_params_shortcode( $elem,  )
+				}
+
+				array_push($this->obj_arr, array('tag'=>$class_dir[0],'shortcode'=>$shortcode);
+				$shortcode = '';
 			}
-
 		}
 
-		if ( is_array($this->class_arr) &&
-			 $this->class_arr !== null && $this->class_arr !== '') {
-
-			foreach ($this->class_arr as $class ) {
-
-				$this->effect_arr = explode( '-', $class );
-
-				if ((! is_empty( $this->effect_arr )) &&
-					 ( is_array( $this->effect_arr ) ) && 
-					 ( $this->effect_arr !== '' ) && ( $this->effect_arr !== false ) ) {
-
-					$effect = $this->effect_arr[0];
-					$direction = $this->effect_arr[1];
-
-				} else {
-					$effect = $class;
-					$direction = false;
-				}
-				
-				array_push( $this->tag_arr, array(
-											'class' => $effect, 
-											'direction' => $direction )
-											);
-
-			}
-
-
-			
-		}*/
+		return $this->obj_arr;
 	}
 
-	/*public function build_elem_shortcodes( $effect_obj ) {
+	public function ece_class_shortcode( $atts ) {
+		$sp = ' ';
+		$ds = '-';
+		$us = '_';
+		$elements = $this->elem_arr;
+		$classes = $this->class_arr;
+		$shortcode = "";
+		$arr = array();
 
-		if ( is_array($effect_obj) && 
-			!is_empty($effect_obj) && 
-			$effect_obj !== null && $effect_obj !== '' ) {
+		$this->params = shortcode_atts( array(
+	        'element' => 'div',
+	        'class' => 'spin360',
+	        'direction' => 'right',
+	        'href' => '#',
+	        'title' => $this->tag_id,
+	        'src' => 'http://placehold.it/350x150',
+	        'value' =>  $this->tag_id,
+	        'name' =>  $this->tag_id,
+	        'id' =>  $this->tag_id,
+	        'alt' =>  $this->tag_id,
+	        'type' => 'text',
+	        'height' => '150',
+	        'action' => 'POST',
+	        'width' => '150',
+	        'style' => 'margin:0 auto;'
+	        				//'class="spin360"'.$sp;
+	    ), $atts );
 
-			$content = 'HTML elements you want effect applied to.';
-			$sp = ' ';
-			$ds = '-';
-			$us = '_';
-			foreach ( $this->elem_arr as $elem ) {
+		$arr_var = $this->ece_element_builder();
 
-				$str = 'div'.$us;
-
-				foreach ( $this->$effect_obj as $effect ) {
-
-					$str .= $elem.$us.$effect['class'].$us.$effect['direction'];
-
-				}
-
-			}
-
-				array_push( $elements , array(
-											'class' => $effect, 
-											'direction' => $direction )
-											);)
-
-			}
+		foreach ( $arr_var as $var ) {
+			return $shortcode;
+		}
 	}
 
-	public function return_elem_shortcode() {
+	public function ece_set_class_array($arr) {
 
+		if ($arr !== null & $arr !== '' && $arr !== false) {
+			$this->obj_arr = $arr;
+		} else {
+
+			$this->obj_arr = null;
+		}
 	}
 
-	public function ece_parse_tag( $tag, $params, $attr_url, $style_html ) {
+	public function ece_get_class_array( ) {
+
+		return $this->obj_arr;
+	}
+
+	public function ece_parse_params_shortcode( $tag, $params ) {
 
 		if ( $tag !== null && $tag !== false && $tag !== false ) {
+
+			$sp = ' ';
+			$ds = '-';
 
 			if ( $tag === 'img' ) {
 
 		    	$img_raw = esc_attr( $params['src'] );
 		    	$img_url = esc_url( $img_raw );
 
-		    	$this->str = '<'. $this->tag.$sp.
-	    					$attr_url.$sp.
+		    	$this->str = '<'. $tag.$sp.
+		    				'id="'. esc_attr( $params['id'] ). '"'.$sp.
+		    				'name="'. esc_attr( $params['id'] ). '"'.$sp.
+		    				'class="'.esc_attr( $params['class'] ).$ds.esc_attr( $params['direction'] ).'"'.$sp.
+		    				'src="'.esc_attr( $params['src'] ).'"'.$sp.
 					    	'alt="'.esc_attr( $params['alt'] ).'"'.$sp.
 					    	'value="'.esc_attr( $params['value'] ).'"'.$sp.
 					    	'height="'.esc_attr( $params['height'] ).'"'.$sp.
@@ -220,10 +214,12 @@ your theme's stylesheet content.
 		    } elseif ( $tag === 'a' ) {
 
 		    	$href_attr = esc_attr( $params['href'] );
-		    	$href_url esc_url( $href_attr );
+		    	$href_url = esc_url( $href_attr );
 
 		    	$this->str = '<'. $this->tag.$sp.
-	    					$attr_url.$sp.
+		    				'id="'. esc_attr( $params['id'] ). '"'.$sp.
+		    				'name="'. esc_attr( $params['id'] ). '"'.$sp.
+		    				'class="'.esc_attr( $params['class'] ).$ds.esc_attr( $params['direction'] ).'"'.$sp.
 					    	'value="'.esc_attr( $params['value'] ).'"'.$sp.
 					    	'href="'.$href_url.'"'.$sp.
 					    	'style="'.$style_html.'"'.$sp.'>'.
@@ -232,7 +228,9 @@ your theme's stylesheet content.
 		    
 		    } elseif ( $tag === 'form' ) {
 		    	$this->str = '<'. $this->tag.$sp.
-		    				$attr_url.$sp.
+		    				'id="'. esc_attr( $params['id'] ). '"'.$sp.
+		    				'name="'. esc_attr( $params['id'] ). '"'.$sp.
+		    				'class="'.esc_attr( $params['class'] ).$ds.esc_attr( $params['direction'] ).'"'.$sp.
 					    	'value="'.esc_attr( $params['value'] ).'"'.$sp.
 					    	'action="'.esc_attr( $params['action'] ).'"'.$sp.
 					    	'style="'.$style_html.'"'.$sp.'>'.
@@ -242,7 +240,9 @@ your theme's stylesheet content.
 		    } elseif ( $tag === 'input' ) {
 
 	    		$this->str = '<'. $this->tag.$sp.
-	    					$attr_url.$sp.
+		    				'id="'. esc_attr( $params['id'] ). '"'.$sp.
+		    				'name="'. esc_attr( $params['id'] ). '"'.$sp.
+		    				'class="'.esc_attr( $params['class'] ).$ds.esc_attr( $params['direction'] ).'"'.$sp.
 					    	'value="'.esc_attr( $params['value'] ).'"'.$sp.
 					    	'type="'.esc_attr( $params['type'] ).'"'.$sp.
 					    	'style="'.$style_html.'"'.$sp.
@@ -250,17 +250,20 @@ your theme's stylesheet content.
 		    } else {
 
 	    		$this->str = '<'. $this->tag.$sp.
-	    					$attr_url.$sp.
+		    				'id="'. esc_attr( $params['id'] ). '"'.$sp.
+		    				'name="'. esc_attr( $params['id'] ). '"'.$sp.
+		    				'class="'.esc_attr( $params['class'] ).$ds.esc_attr( $params['direction'] ).'"'.$sp.
 					    	'value="'.esc_attr( $params['value'] ).'"'.$sp.
 					    	'style="'.$style_html.'"'.$sp.'>'.
 					    	$content.
 					    	'</'. $tag .'>';
 			}
 
-		$shortcode = $this->str;
 
-		return $shortcode;
-	}*/
+		return $this->str;
+	}
+}
+
 }
 
 
